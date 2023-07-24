22243cb3de6d17af3e253f528de7dabc5f52d1bc
Anshul <ayushyadav6a@gmail.com>
2023-07-24 12:27:51 +0530

    Updaing the project

--- src/login/NewPassword.js
+++ src/login/NewPassword.js
@@ -1,5 +1,18 @@
+import { useState } from "react"
 
 
 export const ForgotPassword = () => {
-    return <h1 style={{ margin: "auto" }}>Wleocme to forgot password</h1>
+    const [email, setEmail] = useState("")
+    return (
+        <div style={{ width: "30%", border: "1px solid", margin: "auto", marginTop: "20%", background: "#f7f1ff", textAlign: "center", paddingBottom: "15px" }}>
+            <label htmlFor="myInput" style={{ float: "left", marginTop: "30px", marginLeft: "15px" }}>Email:</label>
+            <input type="email" placeholder="EMAIL" value={email}
+                style={{ width: "50%", marginTop: "25px", borderRadius: "5px" }}
+                id="myInput"
+
+            ></input>
+            <button>Submit</button>
+
+        </div>
+    )
 }
